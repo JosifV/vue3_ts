4c73416d8734b1373a3daeb9b7f804aca671b38b
Your Name <you@example.com>
2020-09-30 15:58:05 +0200

    structure repack

--- vue3_typescript_template/src/App.vue
+++ vue3_typescript_template/src/App.vue
@@ -1,22 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    ABV
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+  name: 'App'
 }
 </script>
+
 
 <style>
 @import "./style/App.scss";
 </style>
-
